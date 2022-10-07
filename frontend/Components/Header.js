e7b608ff3d35b9c3d6d2011e9cbfa6e1801f2b44
Mikel Sigler <mzsigler@gmail.com>
2022-10-07 15:08:04 -0500

    front end kinda coming together

---
+++ frontend/Components/Header.js
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <div className="header">
+            <h2>ECR APPS</h2>
+            <ul>
+                <li>Tax Calculator</li>
+                <li>Maintenance</li>
+                <li>Balance Due</li>
+                <li>Notes</li>
+            </ul>
+        </div>
+    )
+}
---
+++ frontend/pages/page2.js
@@ -0,0 +1,5 @@
+export default function Page2(){
+    return(
+        <h1>Hello I am page 2</h1>
+    )
+}
