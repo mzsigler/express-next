24d32873277a5c52b594e10f5430ed64960eee3c
Mikel Sigler <mzsigler@gmail.com>
2022-10-08 17:27:11 -0500

    building out tax calculator logic

--- frontend/Components/TaxCalculator.js
+++ frontend/Components/TaxCalculator.js
@@ -1,12 +1,36 @@
-
+import { useState } from "react";
 
 
 export default function TaxCalculator(){
+
+    const [price, setPrice] = useState(0)
+
+    function calculate(e){
+        e.preventDefault();
+        const purchasePrice = (e.target.form.price.value);
+        setPrice(purchasePrice);
+        console.log(e.target.form.area.value);
+    }
+
     return(
         <div className="taxCalculator">
 
+            <form name="taxCalcForm" className="taxCalcForm">
+                <label htmlFor="price">Purchase Price</label>
+                <input type="number" name="price" id="price"/>
+                <label htmlFor="area">Select an Area</label>
+                <select name="area" id="area">
+                    <option value="allOther">All Other</option>
+                    <option value="unincorporated">Unincorporated Shelby County</option>
+                </select>
+                <button onClick={calculate}>Submit</button>
+                
 
-            <h2>Tax Calculator Page is me!</h2>
+            </form>
+
+            <div className="taxCalcResults">Price: {price} </div>
+
+            
 
         </div>
     )
--- frontend/pages/notes.js
+++ frontend/pages/notes.js
@@ -8,4 +8,4 @@ export default function notes(){
             <Notes />
         </div>
     )
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
