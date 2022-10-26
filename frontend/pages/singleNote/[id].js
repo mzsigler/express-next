46d92852aa126c8abe06e443cc786fc564488dab
Mikel Sigler <mikelsigler@Mikels-Mac-Studio.local>
2022-10-26 09:06:05 -0500

    finally got that damn post ID

--- frontend/pages/singleNote/[id].js
+++ frontend/pages/singleNote/[id].js
@@ -1,6 +1,11 @@
+import { useRouter } from "next/router";
 import Header from "../../Components/Header";
 
 export default function SingleNote(){
+    const router = useRouter();
+    const { id } = router.query;
+    console.log(id);
+
     return (
         <div className="singleNote">
             <Header />
