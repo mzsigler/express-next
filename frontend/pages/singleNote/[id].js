5f043b42b00acc9365338e053733fd668c5c54e2
Mikel Sigler <mzsigler@gmail.com>
2022-10-27 11:20:04 -0500

    forgot to save

--- frontend/pages/singleNote/[id].js
+++ frontend/pages/singleNote/[id].js
@@ -1,10 +1,28 @@
+import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import Header from "../../Components/Header";
+import gql from "@apollo/client";
 
 export default function SingleNote(){
     const router = useRouter();
     const { id } = router.query;
     console.log(id);
+
+    const SINGLE_POST_QUERY = gql`
+    query($id: ID!){
+  post(where: { id: $id}){
+    title
+    publishDate
+    author {
+      name
+    }
+    content {
+      document
+    }
+  }
+}
+ `
+
 
     return (
         <div className="singleNote">
