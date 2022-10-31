9793c1d9c121c2895e71473f3515d5f63c3d1f8f
Mikel Sigler <mikelsigler@Mikels-Mac-Studio.local>
2022-10-31 12:13:22 -0500

    Working on filter still

--- frontend/Components/CarFilter.js
+++ frontend/Components/CarFilter.js
@@ -9,19 +9,32 @@ const StyledCarFilterForm = styled.form`
 `
 
 export default function CarFilter(){
+
+    let cars;
+    let searchField;
+    let searchTerm;
+
+    function getFormData(e){
+        e.preventDefault();
+        searchField = (e.target.form[1].value).toLowerCase();
+        searchTerm = (e.target.form[0].value).toLowerCase();
+        console.log(searchField, searchTerm);
+    }
+
     return (
         <div className="carFilter">
             <StyledCarFilterForm>
                 <label>Filter</label>
-                <input type="text" name="Inventory" id="inventory" />
+                <input type="text" name="searchText" id="searchText" />
                 <select name="searchBy" id="searchBy">
                     <option value="inv">Inventory Number</option>
                     <option value="make">Make</option>
                     <option value="model">Model</option>
-                    <option value="vin">VIN</option>
                 </select>
+                <button onClick={getFormData}>Go</button>
             </StyledCarFilterForm>
-            
+
+
         </div>
     )
 }
