5eccef1545e5fa5f2d5a95dd0e84089dc2618997
Mikel Sigler <mzsigler@gmail.com>
2022-11-04 16:14:52 -0500

    working on income and expenses

--- frontend/pages/singleCar/[id].js
+++ frontend/pages/singleCar/[id].js
@@ -13,13 +13,19 @@ const SINGLE_CAR_QUERY = gql`
             model,
             vin,
             inv,
+            expense {
+                amount
+            },
+            income {
+                amount
+            }
         }
     }
 `
 
-
-
 export default function SingleCar(){
+
+    let expenseTotal;
 
     const router = useRouter();
     const { id } = router.query;
@@ -30,15 +36,29 @@ export default function SingleCar(){
         }
     });
 
-    console.log(data)
+    if(data){
+        const expenses = data.car.expense;
+        const amounts = expenses.map(expense => expense.amount);
+        console.log(amounts);
+        expenseTotal = amounts.reduce((start, i) => start + i, 0);
+        console.log(expenseTotal);
 
+    }
 
     return(
         <div>
             <Header />
+        {data && 
         <div className="singleCar">
-        
+            <p>{data.car.inv}</p>
+            <p>{data.car.year}</p>
+            <p>{data.car.make}</p>
+            <p>{data.car.model}</p>
+            <p>{data.car.vin}</p>
+            <p>Total Expenses: {expenseTotal}</p>
         </div>
+        }
+
         </div>
     )
 }
