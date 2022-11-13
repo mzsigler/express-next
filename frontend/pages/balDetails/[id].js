53e8c2bf3d82467214a560ca70dcd7197c29af02
Mikel Sigler <mikelsigler@Mikels-MacBook-Pro.local>
2022-11-13 14:46:21 -0600

    balance due almost done

--- frontend/pages/balDetails/[id].js
+++ frontend/pages/balDetails/[id].js
@@ -1,12 +1,63 @@
 import Header from "../../Components/Header";
 import { useRouter } from "next/router";
-import { useQuery } from "@apollo/client";
-import gql from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+import styled from "styled-components";
+
+const BalDueStyle = styled.div`
+    width: 60%;
+    background-color: darkblue;
+    padding: 2rem;
+    margin-left: 20%;
+    border: 1px solid cornflowerblue;
+`
+
+
+const SINGLE_BALANCE_QUERY = gql`
+    query($id: ID!){
+        balance(where: {id: $id}){
+            balanceAmount,
+            name,
+            dateOfBirth,
+            driverLicenseNumber,
+            SSN,
+            phone,
+            employer,
+            employerPhone,
+            localContactName,
+            localContactPhone,
+            returnDate, 
+            notes,
+            payment{
+              paymentAmount,
+              date,
+            },
+        }
+    }`
+
 
 export default function BalDetails(){
 
+    let totalPayments;
+    let paidInFull = false;
+
     const router = useRouter();
     const { id } = router.query;
+
+
+
+    const {data, loading, error} = useQuery(SINGLE_BALANCE_QUERY, {variables:{
+        id
+    }})
+
+    
+    if(data){
+        const payments = data.balance.payment.map(payment => payment.paymentAmount);
+        totalPayments = payments.reduce((start, i) => start + i, 0);
+        if(totalPayments >= data.balance.balanceAmount){
+            paidInFull = true;
+        }
+        
+    }
 
 
 
@@ -14,7 +65,25 @@ export default function BalDetails(){
         <div>
             <Header />
 
-            Hey I am details for balance id {id}.
+            {data && 
+            <BalDueStyle>
+                <h2> <em>{data.balance.name}</em></h2>
+                <p>Amount: ${data.balance.balanceAmount}</p>
+                <p>Return Date: {data.balance.returnDate}</p>
+                <p>DOB: {data.balance.dateOfBirth}</p>
+                <p>License Number: {data.balance.driverLicenseNumber}</p>
+                <p>SSN: {data.balance.SSN}</p>
+                <p>Employer: {data.balance.employer}</p>
+                <p>Employer Phone: {data.balance.employerPhone}</p>
+                <p>Local Contact: {data.balance.localContactName}</p>
+                <p>Local Contact Phone: {data.balance.localContactPhone}</p>
+                <p>Notes: {data.balance.notes}</p>
+                <p>Total Payments: ${totalPayments}</p>
+                <p>Paid in full? {paidInFull === true ? "Yes":"No"}</p>
+
+            </BalDueStyle>}
+            
+            
         </div>
     )
 }
